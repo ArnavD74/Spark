58b6218e533ef1241e33b86439da03f0d8b44bab
Arnav Dashaputra <arugold1@gmail.com>
2019-06-08 14:48:50 -0400

    gjk

--- js/calories.js
+++ js/calories.js
@@ -2,7 +2,6 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
         labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
--- js/inputfoods.js
+++ js/inputfoods.js
@@ -19,18 +19,18 @@ var Niacin;
 var vitaminB6;
 var vitaminB12;
 
-function getData(){
+function getData() {
     
     var foodName = document.getElementById("myText").value;
 
-    if(foodName == 'Apple'){
+    if (foodName == 'Apple') {
         calories = 95;
         totalFat = 0.3;
         saturatedFat = 0.1;
         transFat = 0;
         cholestrol = 0;
         sodium = 1.8;
-        totalCarbohydrates = 25; 
+        totalCarbohydrates = 25;
         dietaryFiber = 4.4;
         totalSugar = 19;
         vitaminA = 2;
@@ -40,22 +40,22 @@ function getData(){
         vitaminK = 0;
         Thiamine = 0;
         Riboflavin = 0;
-        Niacin = 0; 
-        vitaminB6 = 0; 
+        Niacin = 0;
+        vitaminB6 = 0;
         vitaminB12 = 0;
-        protein= 0.5;
+        protein = 0.5;
         score = 86;
         
     }
 
-    if(foodName == 'Banana'){
+    if (foodName == 'Banana') {
         calories = 105;
         totalFat = 0.4;
         saturatedFat = 0.1;
         transFat = 0;
         cholestrol = 0;
         sodium = 1.2;
-        totalCarbohydrates = 27; 
+        totalCarbohydrates = 27;
         dietaryFiber = 3.1;
         totalSugar = 14;
         vitaminA = 1.5;
@@ -65,20 +65,20 @@ function getData(){
         vitaminK = 0;
         Thiamine = 0;
         Riboflavin = 0;
-        Niacin = 0; 
-        vitaminB6 = 0; 
+        Niacin = 0;
+        vitaminB6 = 0;
         vitaminB12 = 0;
         protein = 1.3;
     }
 
-    if(foodName == 'Sprite'){
+    if (foodName == 'Sprite') {
         calories = 140;
         totalFat = 0;
         saturatedFat = 0;
         transFat = 0;
         cholestrol = 0;
         sodium = 65;
-        totalCarbohydrates = 38; 
+        totalCarbohydrates = 38;
         dietaryFiber = 0;
         totalSugar = 38;
         vitaminA = 0;
@@ -115,23 +115,12 @@ function getData(){
     document.getElementById("r18").innerHTML = "Vitamin B12: " + vitaminB12;
     document.getElementById("r19").innerHTML = "Protein: " + protein;
 
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
-    localStorage.setItem("calories", calories);
+    localStorage.setItem("calories", localStorage.getItem("calories") + calories);
+    localStorage.setItem("totalfat", calories);
+    localStorage.setItem("saturatedfat", calories);
+    localStorage.setItem("transfat", calories);
+    localStorage.setItem("cholesterol", calories);
+    localStorage.setItem("sodium", calories);
+    localStorage.setItem("dietaryfiber", calories);
+    localStorage.setItem("sugar", calories);
 }
